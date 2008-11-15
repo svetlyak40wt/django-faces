eeeeb470a5b6812d73dd4404e044af8ca336f6b5
Alexander Artemenko <svetlyak.40wt@gmail.com>
2008-11-15 15:15:47 +0300

    Version 0.1.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = 'django-faces',
-    version = '0.1.3',
+    version = '0.1.4',
     description = '''Django application for nonintrusive avatar support.'''
                   '''It uses site's URL and user's email for autodiscovery.''',
     keywords = 'django apps avatar pavatar gravatar favicon',
